f23d1731ca72240db83e3c3c82f67b7bc45c9891
notamaanmalik <135561389+notamaanmalik@users.noreply.github.com>
2023-08-08 21:01:19 +0530

    Update scanner.py
    Added option to output the results

--- scanner.py
+++ scanner.py
@@ -5,20 +5,21 @@ import socket
 from threading import Thread
 from time import time
 import nmap
+import subprocess
 
 nm = nmap.PortScanner()
 
 open_ports = []
 opentcp = []
-openudp = []
 
 def prepare_args():
     parser = ArgumentParser(description="Python Based Fast Port Scanner",usage="./%(prog)s 192.168.1.1",epilog="Example - %(prog)s -s 200 -e 4000 -t 500 -V 192.168.1.1")
     parser.add_argument(metavar="IPv4",dest="ip",help="host to scan")
     parser.add_argument("-s","--start",dest="start",metavar="",type=int,help="starting port",default=1)
-    parser.add_argument("-e","--end",dest="end",metavar="",type=int,help="ending port",default=100)
-    parser.add_argument("-S","-synack",dest="synack",action="store_true",help="SYN ACK Scan")
+    parser.add_argument("-e","--end",dest="end",metavar="",type=int,help="ending port",default=1000)
+    parser.add_argument("-sS","-syn-ack",dest="synack",action="store_true",help="SYN ACK Scan")
     parser.add_argument("-c","--comprehensive",dest="comp",action="store_true",help="comprehensive scan")
+    parser.add_argument("-o","--output",dest="output",action="store_true",help="save output to the file: result.txt")
     parser.add_argument("-t","--threads",dest="threads",metavar="",type=int,help="threads to use", default=1000)
     args = parser.parse_args()
     return args
@@ -26,6 +27,14 @@ def prepare_args():
 def prepare_ports(start:int,end:int):
     for port in range(start,end+1):
         yield port
+
+def run_port_scanner(ip_address):
+    result = subprocess.run(['./scanner.py', ip_address], capture_output=True, text=True)
+    return result.stdout
+
+def save_to_file(content, filename):
+    with open(filename, 'w') as file:
+        file.write(content)
 
 
 def scan_port():
@@ -84,8 +93,11 @@ if __name__ == "__main__":
     elif arguments.synack:
         comprehensive_scan(arguments.start,arguments.end)
         opentcp = nm[arguments.ip]['tcp'].keys()
-        for bara in opentcp:
-            print("""%s/tcp      open     %s""" % (bara,nm[arguments.ip]['tcp'][bara]['name']))
+        for i in opentcp:
+            print("""%s/tcp      open     %s""" % (i,nm[arguments.ip]['tcp'][i]['name']))
+    elif arguments.output:
+        output = run_port_scanner(arguments.ip)
+        save_to_file(output, 'result.txt')
     else:
         print(f"Open ports found: {open_ports}")
 
